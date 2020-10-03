7aaf2eafbc6d9ce4e94e838a92897660e1e543b8
GeraVM98 <43329532+GeraVM98@users.noreply.github.com>
2020-10-02 19:03:46 -0500

    Punto 2

--- Simulador_IA/MainWindow.xaml.cs
+++ Simulador_IA/MainWindow.xaml.cs
@@ -25,6 +25,8 @@ namespace Simulador_IA
         public string pathApplication = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "../../../"; //root path of the application
         public bool[] isSectionDirty = new bool[2]; //Section A and Section B to check whether is dirty or not
         public System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+        int time = 0, trash = 0, movements = 0;
+        string path = @"Score.txt"; //File path
 
         public MainWindow()
         {            
@@ -32,7 +34,13 @@ namespace Simulador_IA
             
             dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
             dispatcherTimer.Interval = new TimeSpan(0, 0, 5); //TimeSpan(int hours, int min, int secs) Every 5 seconds do dispatcherTimer_Tick
-            dispatcherTimer.Start();
+
+            if (!File.Exists(path)) //Check if file exist
+            {
+                // Create a file to write to.
+                StreamWriter sw = File.CreateText(path);
+            }
+
             initializeSections();
         }
 
@@ -62,27 +70,72 @@ namespace Simulador_IA
             {
                 IMG_SectionB.Source = null; //Remove vaccum from section B
                 IMG_SectionA.Source = new BitmapImage(new Uri(pathApplication + "vaccum.png")); //Put vaccum in section A
+                movements++;
                 if (isSectionDirty[0]) //If section A is dirty
                 {
                     IMG_TrashA.Source = null;
                     isSectionDirty[0] = false;
+                    trash++;
                 }
             }
             else //if section B doesn't have the vaccum
             {
                 IMG_SectionA.Source = null;
                 IMG_SectionB.Source = new BitmapImage(new Uri(pathApplication + "vaccum.png"));
+                movements++;
                 if (isSectionDirty[1])
                 {
                     IMG_TrashB.Source = null;
                     isSectionDirty[1] = false;
+                    trash++;
                 }
+            }
+
+            if (time == 11) //if one minute passed
+            {
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    sw.WriteLine(movements + "," + trash);
+                }
+                dispatcherTimer.Stop();
+            }
+            else
+            {
+                time++;
             }
         }
 
+        private void buttonStart_Click(object sender, RoutedEventArgs e)
+        {
+            time = 0;
+            trash = 0;
+            movements = 0;
+            dispatcherTimer.Start();
+        }
+        private void buttonScore_Click(object sender, RoutedEventArgs e)
+        {
+            // Instantiate window
+            Window1 dialogBox = new Window1();
+            // Show window modally
+            Nullable<bool> dialogResult = dialogBox.ShowDialog();
+        }
         private void button_Click(object sender, RoutedEventArgs e)
         {
 
         }
+        private void buttonClose_Click(object sender, RoutedEventArgs e)
+        {
+            if (dispatcherTimer.IsEnabled) { //If timer is enabled when close the app
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    sw.WriteLine(movements + "," + trash);
+                }
+            }
+            dispatcherTimer.Stop();//Stop timer
+
+            //Close App
+            Application.Current.Shutdown();
+        }
+
     }
 }
---
+++ Simulador_IA/Window1.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Simulador_IA
+{
+    /// <summary>
+    /// Lógica de interacción para Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        string path = @"Score.txt"; //File path
+        int trash = 0, movements = 0, count = 0;
+
+        public Window1()
+        {
+            InitializeComponent();
+
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string[] parts;
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    parts = s.Split(',');
+                    var data = new Score { Movements = parts[0], Trash = parts[1] };
+                    DataGridScore.Items.Add(data);
+                    trash += Convert.ToInt32(parts[1]);
+                    movements += Convert.ToInt32(parts[0]);
+                    count++;
+                }
+            }
+            LBL_Movements.Content = "Movements Average: " + (movements / count).ToString();
+            LBL_Trash.Content = "Trash Average: " + (trash / count).ToString();
+        }
+
+        public class Score
+        {
+            public string Movements { get; set; }
+            public string Trash { get; set; }
+        }
+
+
+        private void buttonClose_Click(object sender, RoutedEventArgs e)
+        {
+            //Close window
+            this.Close();
+        }
+
+    }
+}
